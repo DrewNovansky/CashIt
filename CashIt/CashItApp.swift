aee0ef86898e2157510d75cdfecd62ae3e065ae0
DrewNovansky <novanskyi@gmail.com>
2020-11-23 23:23:34 +0700

    Initial Commit

---
+++ CashIt/CashItApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashItApp.swift
+//  CashIt
+//
+//  Created by Andrew Novansky Ignatius on 23/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CashItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
